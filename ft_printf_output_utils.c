ff8c6122903b1c736906da8db003decaaebc1f91
rafa <ask>
2021-05-25 19:26:24 -0300

    autofix

--- ft_printf_output_utils.c
+++ ft_printf_output_utils.c
@@ -58,10 +58,7 @@ void	ft_putsomething(bool type, char c, char *s, struct var *global)
 		}
 		while (global->flag_zero > 0 && type)
 		{
-			if (global->zero_string)
-				ft_putchar_fd('0', 1);
-			else
-				ft_putchar_fd('0', 1);
+			ft_putchar_fd('0', 1);
 			global->flag_zero--;
 			global->final_size++;
 		}
