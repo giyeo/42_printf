f3a91a11a578deb704aab514dba0a01d46fe1ac1
rafa <ask>
2021-05-25 19:33:38 -0300

    autofix

--- ft_flag.c
+++ ft_flag.c
@@ -6,7 +6,7 @@
 /*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 17:28:21 by rpaulino          #+#    #+#             */
-/*   Updated: 2021/05/25 19:14:28 by rpaulino         ###   ########.fr       */
+/*   Updated: 2021/05/25 19:33:22 by rpaulino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int ft_is_flag(const char *pointer, va_list lista, struct var *global)
 		{
 			global->flag_minus = ft_atoi(&pointer[1]);
 			n += len_int(ft_atoi(&pointer[1]));
+			n += how_many_zeros(&pointer[1]);
 		}
 	}
 	if (pointer[0] == '0')
@@ -71,6 +72,7 @@ int ft_is_flag(const char *pointer, va_list lista, struct var *global)
 		{
 			global->flag_zero = ft_atoi(&pointer[1]);
 			n += len_int(ft_atoi(&pointer[1]));
+			n += how_many_zeros(&pointer[1]);
 		}
 	}
 	return n;
